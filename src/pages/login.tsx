2544615a984315703bf178ad3b876b10507c935c
David <davidemanuel.cf@gmail.com>
2022-01-06 14:21:58 -0300

    Login Page styled

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,33 +1,24 @@
-import { useRouter } from "next/router";
-import { useRef, useState } from "react";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import { FaEye, FaEyeSlash, FaLock, FaUserAlt } from "react-icons/fa";
+import { FaFacebook, FaGlobe, FaInstagram } from "react-icons/fa";
 
 import {
-  Avatar,
   Box,
-  Button,
   chakra,
   Flex,
-  FormControl,
-  FormHelperText,
-  Heading,
+  Grid,
+  GridItem,
+  Icon,
   Image,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement,
   Link,
   Stack,
+  Text,
 } from "@chakra-ui/react";
 
-import { firebase, auth } from "../firebase/clientApp";
-import FirebaseUIAuth from "react-firebaseui-localized";
+import { auth, firebase } from "../firebase/clientApp";
 
-const CFaUserAlt = chakra(FaUserAlt);
-const CFaLock = chakra(FaLock);
-const CFaEyeSlash = chakra(FaEyeSlash);
-const CFaEye = chakra(FaEye);
+const CFaGlobe = chakra(FaGlobe);
+const CFaInstagram = chakra(FaInstagram);
+const CFaFacebook = chakra(FaFacebook);
 
 // Configure FirebaseUI.
 const uiConfig: firebaseui.auth.Config = {
@@ -42,69 +33,90 @@ const uiConfig: firebaseui.auth.Config = {
 };
 
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleShowClick = () => setShowPassword(!showPassword);
-
   return (
-    <Flex
-      flexDirection="column"
-      width="100wh"
-      height="100vh"
-      backgroundColor="blue.100"
-      justifyContent="center"
-      alignItems="center"
+    <Grid
+      templateColumns={["repeat(1, 1fr)", "repeat(1, 1fr)", "repeat(3, 1fr)"]}
+      gap={3}
+      backgroundImage="url('/images/fundo.png')"
+      backgroundRepeat="no-repeat"
+      backgroundAttachment="fixed"
+      backgroundPosition="center"
+      backgroundSize="cover"
     >
-      <Stack flexDir="column" mb="2" justifyContent="center" alignItems="center">
-        <Image
-          src="/images/logo.png"
-          alt="Logo APV Truck"
-          objectFit="contain"
-          boxSize="300px"
-          fallbackSrc="https://via.placeholder.com/300"
-        />
-        <Box minW={{ base: "90%", md: "468px" }}>
-          {/* <form>
-            <Stack spacing={5} p="1rem" backgroundColor="whiteAlpha.900" boxShadow="md">
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement pointerEvents="none">
-                    <CFaUserAlt color="gray.300" />
-                  </InputLeftElement>
-                  <Input type="email" placeholder="Email" />
-                </InputGroup>
-              </FormControl>
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement pointerEvents="none" color="gray.300">
-                    <CFaLock color="gray.300" />
-                  </InputLeftElement>
-                  <Input type={showPassword ? "text" : "password"} placeholder="Senha" />
-                  <InputRightElement width="4.5rem">
-                    <Button h="1.75rem" size="sm" onClick={handleShowClick}>
-                      {showPassword ? <CFaEye /> : <CFaEyeSlash />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                <FormHelperText textAlign="right">
-                  <Link>esqueceu a senha?</Link>
-                </FormHelperText>
-              </FormControl>
-              <Button
-                borderRadius={0}
-                type="submit"
-                variant="solid"
-                colorScheme="teal"
-                width="full"
+      <GridItem
+        w="100%"
+        h="100vh"
+        backgroundImage="url('/images/truck.png')"
+        backgroundSize="cover"
+        backgroundRepeat="no-repeat"
+        backgroundPosition="right"
+        display={["none", "none", "grid"]}
+      />
+      <GridItem w="100%" h="100vh">
+        <Flex
+          flexDirection="column"
+          width="100wh"
+          height="100vh"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Stack flexDir="column" mb="2" justifyContent="center" alignItems="center">
+            <Image
+              src="/images/logo2.png"
+              alt="Logo APV Truck"
+              objectFit="contain"
+              boxSize="300px"
+            />
+            <Box minW={{ base: "90%", md: "468px" }}>
+              <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+            </Box>
+          </Stack>
+          <Box
+            as="footer"
+            role="contentinfo"
+            mx="auto"
+            maxW="7xl"
+            py="12"
+            px={{ base: "4", md: "8" }}
+          >
+            <Box
+              gap={2}
+              display="flex"
+              flexDirection="row"
+              justifyContent="center"
+              alignItems="center"
+              color="white"
+            >
+              <Link
+                display="flex"
+                flexDirection="row"
+                justifyContent="center"
+                alignItems="center"
+                gap={1}
+                href="https://www.apvtruck.com.br"
               >
-                Entrar
-              </Button>
-            </Stack>
-          </form> */}
-          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
-        </Box>
-      </Stack>
-    </Flex>
+                <CFaGlobe />
+                www.apvtruck.com.br
+              </Link>
+              <Text>/</Text>
+              <Text>@apvtruck</Text>
+              <Icon as={CFaInstagram} />
+              <Icon as={CFaFacebook} />
+            </Box>
+          </Box>
+        </Flex>
+      </GridItem>
+
+      <GridItem
+        w="100%"
+        h="100vh"
+        backgroundImage="url('/images/tank.png')"
+        backgroundSize="cover"
+        backgroundRepeat="no-repeat"
+        backgroundPosition="left"
+        display={["none", "none", "grid"]}
+      />
+    </Grid>
   );
 };
 
